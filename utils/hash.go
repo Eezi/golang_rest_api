3b3dc7c55ede03caa49f75330c24efdd9e0341c5
Eezi <tomi.a.salo@outlook.com>
2025-05-11 19:59:24 +0300

    feat: added check valid password

--- utils/hash.go
+++ utils/hash.go
@@ -6,3 +6,8 @@ func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
+
+func CheckPasswordHash(password string, passwordHash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(password))
+	return err == nil
+}
