1c0bc588a3c9dada7f216f9506dae3635fcddd41
Eezi <tomi.a.salo@outlook.com>
2025-05-13 19:58:55 +0300

    feat: added new table for registrations

--- db/db.go
+++ db/db.go
@@ -56,4 +56,20 @@ func createTables() {
 	if err != nil {
 		panic("Could not create events tabele.")
 	}
+
+	createRegistrationsTable := `
+		CREATE TABLE IF NOT EXISTS registrations (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			event_id INTEGER,
+			user_id INTEGER,
+			FOREIGN KEY(event_id) REFEENCES events(id),
+			FOREIGN KEY(user_id) REFEENCES users(id),
+		)
+	`
+
+	_, err = DB.Exec(createRegistrationsTable)
+
+	if err != nil {
+		panic("Could not create registrations tabele.")
+	}
 }
